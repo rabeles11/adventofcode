77aa56e3dab6ce5b73d32635a1c6a3bad0ec8c9c
Rabeles11 <9david@seznam.cz>
2023-12-03 19:31:30 +0100

    refactor

--- 3/firststar.js
+++ 3/firststar.js
@@ -8,6 +8,46 @@ const regexNumber = /\d/g;
 
 let result = 0;
 
+function search(array, index, rightDirection){
+    let search = true;
+    let searchIndex = index;
+    let DesiredNumber = [];
+    while(search){
+            if(array[searchIndex] && array[searchIndex].match(regexNumber)){
+                DesiredNumber.push(array[searchIndex].match(regexNumber)[0])
+                if(rightDirection){
+                    searchIndex++;
+                }
+                else{
+                    searchIndex--;
+                }
+            }
+            else{
+                search = false;
+            }
+    }
+    return DesiredNumber;
+}
+function searchParent(array, parentIndex, index, regexNumber, rightDirection){
+    let search = true;
+    let searchIndex = index;
+    let DesiredNumber = [];
+    while(search){
+        if(array[parentIndex] && array[parentIndex][searchIndex] && array[parentIndex][searchIndex].match(regexNumber)){
+            DesiredNumber.push(array[parentIndex][searchIndex].match(regexNumber)[0]);
+            if(rightDirection){
+                searchIndex++;
+            } else {
+                searchIndex--;
+            }
+        } else {
+            search = false;
+        }
+    }
+    return DesiredNumber;
+}
+
+
 arrayOfArrays.map((itemArray, indexArray) => {
     itemArray.map((item, index) => {
         if(item.match(regex)){
@@ -15,221 +55,35 @@ arrayOfArrays.map((itemArray, indexArray) => {
             let arrayOfNumbersToSum = [];
             if(itemArray[index + 1].match(regexNumber)){
                 searchFinded.right = true;
-                let search = true;
-                let searchIndexRight = index + 1;
-                let DesiredNumber = [];
-                while(search){
-                        if(itemArray[searchIndexRight] && itemArray[searchIndexRight].match(regexNumber)){
-                            DesiredNumber.push(itemArray[searchIndexRight].match(regexNumber)[0])
-                            searchIndexRight++;
-                        }
-                        else{
-                            search = false;
-                        }
-                }
-                arrayOfNumbersToSum.push(Number(DesiredNumber.join('')))
+                arrayOfNumbersToSum.push(Number(search(itemArray, index + 1, true).join('')))
             }
             if(itemArray[index - 1].match(regexNumber)){
                 searchFinded.left = true;
-                let search = true;
-                let searchIndexLeft = index - 1;
-                let DesiredNumber = [];
-
-                while(search){
-                    if(itemArray[searchIndexLeft] && itemArray[searchIndexLeft].match(regexNumber)){
-                        DesiredNumber.push(itemArray[searchIndexLeft].match(regexNumber)[0])
-                        searchIndexLeft--;
-                    }
-                    else{
-                        search = false;
-                    }
-                }
-                arrayOfNumbersToSum.push(Number(DesiredNumber.reverse().join('')))
+                arrayOfNumbersToSum.push(Number(search(itemArray, index - 1, false).reverse().join('')))
             }
             if(arrayOfArrays[indexArray - 1][index].match(regexNumber)){
                 searchFinded.top = true;
-                let search = true;
-                let searchIndexRight = index;
-                let DesiredNumber = [];
-                while(search){
-                        if(arrayOfArrays[indexArray - 1][searchIndexRight] && arrayOfArrays[indexArray - 1][searchIndexRight].match(regexNumber)){
-                            DesiredNumber.push(arrayOfArrays[indexArray - 1][searchIndexRight].match(regexNumber)[0])
-                            searchIndexRight++;
-                        }
-                        else{
-                            search = false;
-                        }
-                }
-
-                let searchLeft = true;
-                let searchIndexLeft = index - 1;
-                let DesiredNumber2 = [];
-
-                while(searchLeft){
-                    if(arrayOfArrays[indexArray - 1][searchIndexLeft] && arrayOfArrays[indexArray - 1][searchIndexLeft].match(regexNumber)){
-                        DesiredNumber2.push(arrayOfArrays[indexArray - 1][searchIndexLeft].match(regexNumber)[0])
-                        searchIndexLeft--;
-                    }
-                    else{
-                        searchLeft = false;
-                    }
-                }
-                arrayOfNumbersToSum.push(Number(DesiredNumber2.reverse().concat(DesiredNumber).join('')))
+                arrayOfNumbersToSum.push(Number(searchParent(arrayOfArrays,indexArray-1,index-1,regexNumber,false).reverse().concat(searchParent(arrayOfArrays,indexArray-1,index,regexNumber,true)).join('')))
             }
             if(arrayOfArrays[indexArray + 1][index].match(regexNumber)){
                 searchFinded.bottom = true;
-
-                let search = true;
-                let searchIndexRight = index;
-                let DesiredNumber = [];
-                while(search){
-                        if(arrayOfArrays[indexArray + 1][searchIndexRight] && arrayOfArrays[indexArray + 1][searchIndexRight].match(regexNumber)){
-                            DesiredNumber.push(arrayOfArrays[indexArray + 1][searchIndexRight].match(regexNumber)[0])
-                            searchIndexRight++;
-                        }
-                        else{
-                            search = false;
-                        }
-                }
-
-                let searchLeft = true;
-                let searchIndexLeft = index - 1;
-                let DesiredNumber2 = [];
-
-                while(searchLeft){
-                    if(arrayOfArrays[indexArray + 1][searchIndexLeft] && arrayOfArrays[indexArray + 1][searchIndexLeft].match(regexNumber)){
-                        DesiredNumber2.push(arrayOfArrays[indexArray + 1][searchIndexLeft].match(regexNumber)[0])
-                        searchIndexLeft--;
-                    }
-                    else{
-                        searchLeft = false;
-                    }
-                }
-                arrayOfNumbersToSum.push(Number(DesiredNumber2.reverse().concat(DesiredNumber).join('')))
+                arrayOfNumbersToSum.push(Number(searchParent(arrayOfArrays,indexArray+1,index-1,regexNumber,false).reverse().concat(searchParent(arrayOfArrays,indexArray+1,index,regexNumber,true)).join('')))
             }
             if(arrayOfArrays[indexArray + 1][index + 1].match(regexNumber) && !searchFinded.bottom){
                 searchFinded.bottomRight = true;
-
-                let search = true;
-                let searchIndexRight = index + 1;
-                let DesiredNumber = [];
-                while(search){
-                        if(arrayOfArrays[indexArray + 1][searchIndexRight] && arrayOfArrays[indexArray + 1][searchIndexRight].match(regexNumber)){
-                            DesiredNumber.push(arrayOfArrays[indexArray + 1][searchIndexRight].match(regexNumber)[0])
-                            searchIndexRight++;
-                        }
-                        else{
-                            search = false;
-                        }
-                }
-
-                let searchLeft = true;
-                let searchIndexLeft = index + 1 - 1;
-                let DesiredNumber2 = [];
-
-                while(searchLeft){
-                    if(arrayOfArrays[indexArray + 1][searchIndexLeft] && arrayOfArrays[indexArray + 1][searchIndexLeft].match(regexNumber)){
-                        DesiredNumber2.push(arrayOfArrays[indexArray + 1][searchIndexLeft].match(regexNumber)[0])
-                        searchIndexLeft--;
-                    }
-                    else{
-                        searchLeft = false;
-                    }
-                }
-                arrayOfNumbersToSum.push(Number(DesiredNumber2.reverse().concat(DesiredNumber).join('')))
+                arrayOfNumbersToSum.push(Number(searchParent(arrayOfArrays,indexArray+1,index+1-1,regexNumber,false).reverse().concat(searchParent(arrayOfArrays,indexArray+1,index+1,regexNumber,true)).join('')))
             }
             if(arrayOfArrays[indexArray + 1][index - 1].match(regexNumber) && !searchFinded.bottom){
                 searchFinded.bottomLeft = true;
-
-                let search = true;
-                let searchIndexRight = index - 1;
-                let DesiredNumber = [];
-                while(search){
-                        if(arrayOfArrays[indexArray + 1][searchIndexRight] && arrayOfArrays[indexArray + 1][searchIndexRight].match(regexNumber)){
-                            DesiredNumber.push(arrayOfArrays[indexArray + 1][searchIndexRight].match(regexNumber)[0])
-                            searchIndexRight++;
-                        }
-                        else{
-                            search = false;
-                        }
-                }
-
-                let searchLeft = true;
-                let searchIndexLeft = index - 1 - 1;
-                let DesiredNumber2 = [];
-
-                while(searchLeft){
-                    if(arrayOfArrays[indexArray + 1][searchIndexLeft] && arrayOfArrays[indexArray + 1][searchIndexLeft].match(regexNumber)){
-                        DesiredNumber2.push(arrayOfArrays[indexArray + 1][searchIndexLeft].match(regexNumber)[0])
-                        searchIndexLeft--;
-                    }
-                    else{
-                        searchLeft = false;
-                    }
-                }
-                arrayOfNumbersToSum.push(Number(DesiredNumber2.reverse().concat(DesiredNumber).join('')))
+                arrayOfNumbersToSum.push(Number(searchParent(arrayOfArrays,indexArray+1,index-1-1,regexNumber,false).reverse().concat(searchParent(arrayOfArrays,indexArray+1,index-1,regexNumber,true)).join('')))
             }
             if(arrayOfArrays[indexArray - 1][index + 1].match(regexNumber) && !searchFinded.top){
                 searchFinded.topRight = true;
-
-                let search = true;
-                let searchIndexRight = index + 1;
-                let DesiredNumber = [];
-                while(search){
-                        if(arrayOfArrays[indexArray - 1][searchIndexRight] && arrayOfArrays[indexArray - 1][searchIndexRight].match(regexNumber)){
-                            DesiredNumber.push(arrayOfArrays[indexArray - 1][searchIndexRight].match(regexNumber)[0])
-                            searchIndexRight++;
-                        }
-                        else{
-                            search = false;
-                        }
-                }
-
-                let searchLeft = true;
-                let searchIndexLeft = index + 1 - 1;
-                let DesiredNumber2 = [];
-
-                while(searchLeft){
-                    if(arrayOfArrays[indexArray - 1][searchIndexLeft] && arrayOfArrays[indexArray - 1][searchIndexLeft].match(regexNumber)){
-                        DesiredNumber2.push(arrayOfArrays[indexArray - 1][searchIndexLeft].match(regexNumber)[0])
-                        searchIndexLeft--;
-                    }
-                    else{
-                        searchLeft = false;
-                    }
-                }
-                arrayOfNumbersToSum.push(Number(DesiredNumber2.reverse().concat(DesiredNumber).join('')))
+                arrayOfNumbersToSum.push(Number(searchParent(arrayOfArrays,indexArray-1,index+1-1,regexNumber,false).reverse().concat(searchParent(arrayOfArrays,indexArray-1,index+1,regexNumber,true)).join('')))
             }
             if(arrayOfArrays[indexArray - 1][index - 1].match(regexNumber) && !searchFinded.top){
                 searchFinded.topLeft = true;
-
-                let search = true;
-                let searchIndexRight = index - 1;
-                let DesiredNumber = [];
-                while(search){
-                        if(arrayOfArrays[indexArray - 1][searchIndexRight] && arrayOfArrays[indexArray - 1][searchIndexRight].match(regexNumber)){
-                            DesiredNumber.push(arrayOfArrays[indexArray - 1][searchIndexRight].match(regexNumber)[0])
-                            searchIndexRight++;
-                        }
-                        else{
-                            search = false;
-                        }
-                }
-
-                let searchLeft = true;
-                let searchIndexLeft = index - 1 - 1;
-                let DesiredNumber2 = [];
-
-                while(searchLeft){
-                    if(arrayOfArrays[indexArray - 1][searchIndexLeft] && arrayOfArrays[indexArray - 1][searchIndexLeft].match(regexNumber)){
-                        DesiredNumber2.push(arrayOfArrays[indexArray - 1][searchIndexLeft].match(regexNumber)[0])
-                        searchIndexLeft--;
-                    }
-                    else{
-                        searchLeft = false;
-                    }
-                }
-                arrayOfNumbersToSum.push(Number(DesiredNumber2.reverse().concat(DesiredNumber).join('')))
+                arrayOfNumbersToSum.push(Number(searchParent(arrayOfArrays,indexArray-1,index -1-1,regexNumber,false).reverse().concat(searchParent(arrayOfArrays,indexArray-1,index-1,regexNumber,true)).join('')))
             }
 
             result += arrayOfNumbersToSum.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
